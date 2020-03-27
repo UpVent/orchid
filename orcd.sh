ce6cd4963f8a38090db2ab0571106ab4c2f6ef29
VentGrey <omarpurataf@gmail.com>
2020-03-26 18:50:12 -0600

    Delete colours, real unix programs don't do that

--- orcd.sh
+++ orcd.sh
@@ -1,42 +1,5 @@
 #! /usr/bin/env bash
 
-# Variables
-# Colors
-Black='\e[0;30m'        # Black
-Red='\e[0;31m'          # Red
-Green='\e[0;32m'        # Green
-Yellow='\e[0;33m'       # Yellow
-Blue='\e[0;34m'         # Blue
-Purple='\e[0;35m'       # Purple
-Cyan='\e[0;36m'         # Cyan
-White='\e[0;37m'        # White
-
-# Bold
-BBlack='\e[1;30m'       # Black
-BRed='\e[1;31m'         # Red
-BGreen='\e[1;32m'       # Green
-BYellow='\e[1;33m'      # Yellow
-BBlue='\e[1;34m'        # Blue
-BPurple='\e[1;35m'      # Purple
-BCyan='\e[1;36m'        # Cyan
-BWhite='\e[1;37m'       # White
-
-# Default
-DefaultColor='\e[39m'   # Default foreground color
-
-b='\033[1m'
-u='\033[4m'
-bl='\E[30m'
-r='\E[31m'
-g='\E[32m'
-y='\E[33m'
-bu='\E[34m'
-m='\E[35m'
-c='\E[36m'
-w='\E[37m'
-endc='\E[0m'
-enda='\033[0m'
-
 # TODO: Implement this function
 function distro_check {
     if type lsb_release >/dev/null 2>&1 ; then
